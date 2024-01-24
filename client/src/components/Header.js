decc2ab17e0762b9cf6e0265b054a0c3490fea95
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-24 14:28:02 +0300

    Add logo, styling, and dropdown menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import '../App.css';
+import logo from '../logo2.jpeg';
 
 function Header() {
   return (
-    <header>
-      <div id="logo">Logo</div>
-      <div id="actions">Actions</div>
-      <div id="preferences">Preferences</div>
+    <header className="header">
+      <div className="logo">
+      <img src={logo} alt="Logo" className="logo-img" />
+      </div>
+      <div className="dropdown">
+        <button className="dropbtn">Actions</button>
+        <div className="dropdown-content">
+          <a href="#">Alerts</a>
+          <a href="#">Trades</a>
+          <a href="#">Wallet</a>
+          <a href="#">Watchlist</a>
+        </div>
+      </div>
+      <div className="preferences">Preferences</div>
     </header>
   );
 }
