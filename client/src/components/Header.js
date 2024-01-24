38fc049d9d8d47cb2fd84b9f6225c626ee324693
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-24 20:40:41 +0300

    Enhance Header component with navigation and theme toggle
    Add useHistory from react-router-dom for navigation
    
    Add handleActionClick function to navigate to login on action item click
    
    Add handleLoginSignupClick function to navigate to login on button click
    
    Add toggleTheme function to switch themes
    
    Modify dropdown to include theme switch and login/signup buttons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import '../App.css';
 import logo from '../logo2.jpeg';
 
-function Header() {
+function Header({ toggleTheme }) {
+  const history = useHistory();
+
+  const handleActionClick = () => {
+    history.push('/login');
+  };
+
+  const handleLoginSignupClick = () => {
+    history.push('/login');
+  };
+
   return (
     <header className="header">
       <div className="logo">
-      <img src={logo} alt="Logo" className="logo-img" />
+        <img src={logo} alt="Logo" className="logo-img" />
       </div>
       <div className="dropdown">
         <button className="dropbtn">Actions</button>
         <div className="dropdown-content">
-          <a href="#">Alerts</a>
-          <a href="#">Trades</a>
-          <a href="#">Wallet</a>
-          <a href="#">Watchlist</a>
+          <a href="#" onClick={handleActionClick}>Alerts</a>
+          <a href="#" onClick={handleActionClick}>Trades</a>
+          <a href="#" onClick={handleActionClick}>Wallet</a>
+          <a href="#" onClick={handleActionClick}>Watchlist</a>
         </div>
       </div>
-      <div className="preferences">Preferences</div>
+      <div className="dropdown-preferences">
+        Preferences
+        <div className="dropdown-content-preferences">
+          <button onClick={toggleTheme}>Switch theme</button>
+          <button onClick={handleLoginSignupClick}>Login/Signup</button>
+        </div>
+      </div>
     </header>
   );
 }
