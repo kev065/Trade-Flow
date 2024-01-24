a4ccdbe87a14bf720a064ddc052ddb7787dd47fd
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-24 11:54:05 +0300

    Add CSS styling to LandingPage and OrderForm components

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import Header from './components/Header';
 import LandingPage from './components/LandingPage';
 import OrderForm from './components/OrderForm';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <LandingPage />
-      <OrderForm />
+      <div className="chart">
+        <LandingPage />
+      </div>
+      <div className="order-form">
+        <OrderForm />
+      </div>
     </div>
   );
 }
+
 
 export default App;
 
