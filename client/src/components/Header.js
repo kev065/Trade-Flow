bf74ae5853ccaeb7820c5a906e14476ab2165cab
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-24 05:17:19 +0300

    Add Header component with logo, actions, and preferences

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div id="logo">Logo</div>
+      <div id="actions">Actions</div>
+      <div id="preferences">Preferences</div>
+    </header>
+  );
+}
+
+export default Header;
