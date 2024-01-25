a7b18695a68a5715ea7e6e728830c20c047819c6
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-25 06:30:48 +0300

    Add login functionality
    Add ability to log in via a POST request to the server
    
    If the user is authenticated, the 'isLoggedIn' state is set to true and the user is redirected to the order form page
    
    If the user is not authenticated, they are redirected to the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Header from './components/Header';
 import LandingPage from './components/LandingPage';
 import OrderForm from './components/OrderForm';
+import Login from './components/Login';
 import './light.css';
 import './dark.css';
 
 function App() {
-  const [theme, setTheme] = useState('light'); // default theme is light
+  const [theme, setTheme] = useState('light'); 
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
 
   const toggleTheme = () => {
     if (theme === 'light') {
@@ -17,18 +20,24 @@ function App() {
   };
 
   return (
-    <div className={`App ${theme}`}>
-      <Header toggleTheme={toggleTheme} />
-      <div className="chart">
-        <LandingPage />
+    <Router>
+      <div className={`App ${theme}`}>
+        <Header toggleTheme={toggleTheme} isLoggedIn={isLoggedIn} />
+        <div className="chart">
+          <LandingPage />
+        </div>
+        <div className="order-form">
+          {isLoggedIn ? <OrderForm /> : <Redirect to="/login" />}
+        </div>
+        <Route path="/login">
+          <Login setIsLoggedIn={setIsLoggedIn} />
+        </Route>
       </div>
-      <div className="order-form">
-        <OrderForm />
-      </div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
 
 
