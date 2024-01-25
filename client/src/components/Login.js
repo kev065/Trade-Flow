f3eb9f193ff15ac7213d3847f01195e2776a2ab9
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-25 08:45:32 +0300

    Enhance Login component with navigation
    Import 'useNavigate' from 'react-router-dom' to handle navigation
    
    Add 'navigate' function to 'handleSubmit' method
    
    If user is authenticated, navigate to the homepage after login
    
    To fix - Login 'username' and 'password' field render at bottom of homepage when clicked. Working on moving to separate page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Login({ setIsLoggedIn }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -16,6 +18,7 @@ function Login({ setIsLoggedIn }) {
       if (response.data.authenticated) {
         setIsLoggedIn(true);
         localStorage.setItem('token', response.data.token);
+        navigate('/'); 
       } else {
         alert('Invalid username or password');
       }
